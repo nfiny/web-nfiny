78ead639464f1628d031fd112343c06e04c55a6c
Jae <jae@nfiny.com>
2022-02-05 22:58:47 +1100

    Small updates to submit logic

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,22 +1,20 @@
 jQuery(document).ready(function($) {
     "use strict";
-  
+
     //Contact
     $('form.contactForm').submit(function() {
       var action = $(this).attr('action');
       var str = $(this).serialize();
-
-      $('.submit-success').removeAttr('hidden')
-      $('#submitBtn').hide()
 
       $.ajax({
         type: "POST",
         url: action,
         data: str,
         success: function(msg) {
+          $('.submit-success').removeAttr('hidden')
+          $('#submitBtn').hide()
         },
         error: function(msg) {
-          alert('There was an error submitting the request. Try again.')
         }
       });
       return false;
