3976a01c4f381fa6b9e36e3ed4bfdc6ddd05a045
DESKTOP-85PJF9K\young <youngjae_nam@hotmail.com>
2022-02-05 22:19:42 +1100

    test

--- js/contactForm.js
+++ js/contactForm.js
@@ -6,15 +6,19 @@ jQuery(document).ready(function($) {
       var action = $(this).attr('action');
       var str = $(this).serialize();
 
+      $('.submit-loading').removeAttr('hidden')
+      $('#submitBtn').hide()
+
       $.ajax({
         type: "POST",
         url: action,
         data: str,
         success: function(msg) {
-            alert('success');
         },
         error: function(msg) {
             alert('Send contact form failed cos: ' + msg);
+            $('.submit-loading').hide()
+            $('.submit-success').removeAttr('hidden')
         }
       });
       return false;
