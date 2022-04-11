47b748cf5e2f198a5b948eb81abdf7a586e279df
pon <pon@nfiny.com>
2022-04-11 23:33:56 +1000

    finalize captchav3

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,29 +1,42 @@
 jQuery(document).ready(function($) {
     "use strict";
 
-   //Contact
-    $('form.contactForm').submit(function() {
-      var action = $(this).attr('action');
-      var str = $(this).serialize();
-
-      $('input.submit-loading').removeAttr('hidden')
-      $('#submitBtn').hide()
-
-      $.ajax({
-        type: "POST",
-        url: action,
-        data: str,
-        success: function(msg) {
-          $('.submit-success').removeAttr('hidden')
-          $('#submitBtn').hide()
-        },
-        error: function(msg) {
-          $('.submit-error').removeAttr('hidden');
-          $('#submitBtn').hide();
-          $('input.submit-loading').hide();
-        }
-      });
-      return false;
+  // Captcha
+  grecaptcha.ready(function() {
+    grecaptcha.execute('6LcpMmUfAAAAACyGT5nkxsPx7Q2aGTsDU2AGQj_M', {action:'validate_captcha'})
+              .then(function(token) {
+        // add token value to form
+        document.getElementById('g-recaptcha-response').value = token;
     });
-  
   });
+
+  // Contact
+  $('form.contactForm').submit(function(e) {
+    e.preventDefault();
+
+    var action = $(this).attr('action');
+    var str = $(this).serialize();
+
+    $('input.submit-loading').removeAttr('hidden');
+    $('#submitBtn').hide();
+
+    alert(str)  
+    $.ajax({
+      type: "POST",
+      url: action,
+      data: str,
+      success: function(msg) {
+        $('.submit-success').removeAttr('hidden')
+        $('#submitBtn').hide()
+      },
+      error: function(msg) {
+        alert(JSON.stringify(msg));
+        $('.submit-error').removeAttr('hidden');
+        $('#submitBtn').hide();
+        $('input.submit-loading').hide();
+      }
+    });
+    return false;
+  });
+
+});
